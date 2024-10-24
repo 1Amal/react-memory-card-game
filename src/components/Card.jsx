cd80f447e0148293d5f4f20459d19a2293dfda04
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-10-25 09:44:26 +1100

    Add Giphy image ID's and update fetch image code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,30 @@
+import {useState} from "react";
 export default function Card() {
-  const fetchedImage2 =
-    "https://media1.giphy.com/media/rIq6ASPIqo2k0/giphy.gif?cid=cfdad64aqw073qig3wpx2dr0xin5g47sxnua9eg9uwupolsl&ep=v1_gifs_translate&rid=giphy.gif&ct=g";
+
+  const [fetchedImage, setFetchedImage] = useState("")
 
   function testImage() {
-    const fetchedImage =
+    const fetchedImageSimulation =
       "https://media1.giphy.com/media/rIq6ASPIqo2k0/giphy.gif?cid=cfdad64aqw073qig3wpx2dr0xin5g47sxnua9eg9uwupolsl&ep=v1_gifs_translate&rid=giphy.gif&ct=g";
 
-    console.log(fetchedImage);
-    <img src="https://media1.giphy.com/media/rIq6ASPIqo2k0/giphy.gif?cid=cfdad64aqw073qig3wpx2dr0xin5g47sxnua9eg9uwupolsl&ep=v1_gifs_translate&rid=giphy.gif&ct=g" />;
-    return fetchedImage;
+      setFetchedImage(fetchedImageSimulation);
+        console.log(fetchedImage);
+
+    
+    // return fetchedImage;
   }
   return (
     <div className="projectCardsContainer">
       <div className="projectDiv">
         <h1>Card 1</h1>
-        <img
+        {/* <img
           src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmcwZHU0YWR0eWM0anIwenp3ZTRpc2FwZHBrYnlhZzJ5dTFqbjhzZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13FrpeVH09Zrb2/giphy.gif"
           alt="CSS"
-        />
-
-        {/* <img src={()=>testImage()} alt="Test Image"/>
-         */}
+        /> */}
+        
+        <button onClick={testImage}>Fetch Image</button>
+        <img src={fetchedImage} alt="Test Image"/>
+        
 
       </div>
 
--- src/components/GameLogic.jsx
+++ src/components/GameLogic.jsx
@@ -1,35 +1,44 @@
+import { useState } from "react";
 export default function GameLogic() {
-    async function FetchImage(imageRequired) {
-    // alert(imageRequired);
-    // ("https://api.giphy.com/v1/gifs/translate?api_key=il0jSOJwebRzCIT6pUUgIfQBOZjvpotK&s=Andorid");
+  const [fetchedImage, setFetchedImage] = useState("");
+  //   https://api.giphy.com/v1/gifs/translate?api_key=il0jSOJwebRzCIT6pUUgIfQBOZjvpotK&s=CSS
 
-    // URL (required), options (optional)
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=il0jSOJwebRzCIT6pUUgIfQBOZjvpotK&s=Andorid',{
-    mode:'cors'
-})
-.then(function(response) {
-return response.json();
-// console.log(response.json());
+  //<iframe src="https://giphy.com/embed/M4NykXxUE0HAcK7UJ6" width="480" height="480" style="" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/shecodesio-computer-javascript-shecodes-M4NykXxUE0HAcK7UJ6">via GIPHY</a></p>
+  async function FetchImage(imageRequired) {
+    let imageID = "";
+    const imageIDCSS = "fsEaZldNC8A1PJ3mwp";
+    const imageIDHTML = "XAxylRMCdpbEWUAvr8";
+    const ImageIDJS = "ln7z2eWriiQAllfVcn";
+    const ImageIDReact = "eNAsjO55tPbgaor7ma";
+    const ImageIDGit = "kH1DBkPNyZPOk0BxrM";
 
-})
-.then(function(response){
-    console.log(response.data.images.original.url);
-    // return(<img src='response.data.images.original.url'/>)
-    // return response.data.images.original.url
-    return (response.data.images.original.url)
-})
-.catch(function(err) {
-  console.log(err)
-});
-
+    imageID = ImageIDGit;
+    fetch(
+      `https://api.giphy.com/v1/gifs/${imageID}?api_key=il0jSOJwebRzCIT6pUUgIfQBOZjvpotK&rating=g`,
+      {
+        mode: "cors",
+      }
+    )
+      .then(function (response) {
+        console.dir(response.json);
+        return response.json();
+      })
+      .then(function (response) {
+        console.log(response.data.images.original.url);
+        let returnedImageURL = response.data.images.original.url;
+        setFetchedImage(returnedImageURL);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
   return (
     <div>
       <h1>Game Logic Component</h1>
 
-      <button onClick={()=>FetchImage()}>Fetch Image</button>
+      <button onClick={() => FetchImage()}>Fetch Image</button>
       {/* <p>{console.log(FetchImage)}</p> */}
-      <img src={FetchImage} alt="Fetched Image"/>
+      <img src={fetchedImage} alt="Fetched Image" />
     </div>
   );
 }
